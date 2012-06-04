a20c6197d4a792746cb4ae007c1a2e79d32b8128
MaxK <maxk2005@gmail.com>
2012-06-03 21:51:40 -0700

    Add localization support

---
+++ TripPoint.WindowsPhone/I18N/Localization.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TripPoint.WindowsPhone.I18N
+{
+    public class Localization
+    {
+        private Resources _resources;
+
+        public Localization()
+        {
+            _resources = new Resources();
+        }
+
+        public Resources Resources
+        {
+            get
+            {
+                return _resources;
+            }
+        }
+    }
+}
