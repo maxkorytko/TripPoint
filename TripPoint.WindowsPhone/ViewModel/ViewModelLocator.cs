7ce3bbdc3ed67aa4c0b1433373ae56d7948abb4b
MaxK <maxk2005@gmail.com>
2012-06-03 18:24:13 -0700

    Add CreateTripViewModel

--- TripPoint.WindowsPhone/ViewModel/ViewModelLocator.cs
+++ TripPoint.WindowsPhone/ViewModel/ViewModelLocator.cs
@@ -1,53 +1,36 @@
-﻿/*
-  In App.xaml:
-  <Application.Resources>
-      <vm:GlobalViewModelLocator xmlns:vm="clr-namespace:AcademyLive.ViewModels"
-                                   x:Key="Locator" />
-  </Application.Resources>
-  
-  In the View:
-  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
-*/
-
-namespace TripPoint.WindowsPhone.ViewModel
+﻿namespace TripPoint.WindowsPhone.ViewModel
 {
-    /// <summary>
-    /// This class contains static references to all the view models in the
-    /// application and provides an entry point for the bindings.
-    /// <para>
-    /// Use the <strong>mvvmlocatorproperty</strong> snippet to add ViewModels
-    /// to this locator.
-    /// </para>
-    /// <para>
-    /// In Silverlight and WPF, place the GlobalViewModelLocator in the App.xaml resources:
-    /// </para>
-    /// <code>
-    /// &lt;Application.Resources&gt;
-    ///     &lt;vm:GlobalViewModelLocator xmlns:vm="clr-namespace:AcademyLive.ViewModels"
-    ///                                  x:Key="Locator" /&gt;
-    /// &lt;/Application.Resources&gt;
-    /// </code>
-    /// <para>
-    /// Then use:
-    /// </para>
-    /// <code>
-    /// DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
-    /// </code>
-    /// <para>
-    /// You can also use Blend to do all this with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm/getstarted
-    /// </para>
-    /// </summary>
     public class ViewModelLocator
     {
+        private static CreateTripViewModel _createTripViewModel;
+
         /// <summary>
         /// Initializes a new instance of the GlobalViewModelLocator class.
         /// </summary>
         public ViewModelLocator()
         {
-            
+            InitCreateTripViewModel();
+        }
+
+        public static CreateTripViewModel CreateTripViewModel
+        {
+            get
+            {
+                InitCreateTripViewModel();
+                return _createTripViewModel;
+            }
+        }
+
+        private static void InitCreateTripViewModel()
+        {
+            if (_createTripViewModel == null)
+                _createTripViewModel = new CreateTripViewModel();
+        }
+
+        private static void ClearCreateTripViewModel()
+        {
+            _createTripViewModel.Cleanup();
+            _createTripViewModel = null;
         }
 
         /// <summary>
@@ -55,7 +38,7 @@ namespace TripPoint.WindowsPhone.ViewModel
         /// </summary>
         public static void Cleanup()
         {
-            
+            ClearCreateTripViewModel();
         }
     }
 }
