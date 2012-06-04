a4b7015463e17a360d837c03230eb74eedb5836d
MaxK <maxk2005@gmail.com>
2012-06-03 17:51:40 -0700

    Create the skeleton for view model locator

---
+++ TripPoint.WindowsPhone/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,61 @@
+﻿/*
+  In App.xaml:
+  <Application.Resources>
+      <vm:GlobalViewModelLocator xmlns:vm="clr-namespace:AcademyLive.ViewModels"
+                                   x:Key="Locator" />
+  </Application.Resources>
+  
+  In the View:
+  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+*/
+
+namespace TripPoint.WindowsPhone.ViewModel
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// <para>
+    /// Use the <strong>mvvmlocatorproperty</strong> snippet to add ViewModels
+    /// to this locator.
+    /// </para>
+    /// <para>
+    /// In Silverlight and WPF, place the GlobalViewModelLocator in the App.xaml resources:
+    /// </para>
+    /// <code>
+    /// &lt;Application.Resources&gt;
+    ///     &lt;vm:GlobalViewModelLocator xmlns:vm="clr-namespace:AcademyLive.ViewModels"
+    ///                                  x:Key="Locator" /&gt;
+    /// &lt;/Application.Resources&gt;
+    /// </code>
+    /// <para>
+    /// Then use:
+    /// </para>
+    /// <code>
+    /// DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+    /// </code>
+    /// <para>
+    /// You can also use Blend to do all this with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm/getstarted
+    /// </para>
+    /// </summary>
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Initializes a new instance of the GlobalViewModelLocator class.
+        /// </summary>
+        public ViewModelLocator()
+        {
+            
+        }
+
+        /// <summary>
+        /// Cleans up all the resources.
+        /// </summary>
+        public static void Cleanup()
+        {
+            
+        }
+    }
+}
