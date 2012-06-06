26aea27ad8fe3197140d6b8e4cbd8d322f2c1bec
MaxK <maxk2005@gmail.com>
2012-06-05 19:23:35 -0700

    Create Logger class

---
+++ TripPoint.Model/Utils/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TripPoint.Model.Utils
+{
+    public class Logger
+    {
+        public static void Log(String message)
+        {
+            WriteLogString("{0}", message);
+        }
+
+        public static void Log(string tag, string message)
+        {
+            WriteLogString("{0}::{1}", tag, message);
+        }
+
+        private static void WriteLogString(String format, params object[] args)
+        {
+            System.Diagnostics.Debug.WriteLine(format, args);
+        }
+    }
+}
