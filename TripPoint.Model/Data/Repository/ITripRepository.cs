fa70e504ea650d1e36a39fbb0f3317025cf625b6
MaxK <maxk2005@gmail.com>
2012-06-09 12:21:05 -0700

    Crate ITripRepository interface
    Add memory trip repository

---
+++ TripPoint.Model/Data/Repository/ITripRepository.cs
@@ -0,0 +1,13 @@
+﻿using TripPoint.Model.Domain;
+
+namespace TripPoint.Model.Data.Repository
+{
+    public interface ITripRepository
+    {
+        /// <summary>
+        /// Persists trip to the data store
+        /// </summary>
+        /// <param name="trip">Trip object to persist</param>
+        void SaveTrip(Trip trip);
+    }
+}
---
+++ TripPoint.Model/Data/Repository/Memory/MemoryTripRepository.cs
@@ -0,0 +1,19 @@
+﻿#region SDK Usings
+
+using System;
+
+#endregion
+
+using TripPoint.Model.Domain;
+
+namespace TripPoint.Model.Data.Repository.Memory
+{
+    public class MemoryTripRepository : ITripRepository
+    {
+        public void SaveTrip(Trip trip)
+        {
+            if (trip == null)
+                throw new ArgumentNullException("trip");
+        }
+    }
+}
