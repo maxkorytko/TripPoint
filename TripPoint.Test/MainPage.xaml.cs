75d93b0a65046e295f003a233ff2fa215cc1254c
MaxK <maxk2005@gmail.com>
2012-06-09 13:07:04 -0700

    Create unit test project

---
+++ TripPoint.Test/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Phone.Controls;
+using Microsoft.Silverlight.Testing;
+
+namespace TripPoint.Test
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            const bool runUnitTests = true;
+            if (runUnitTests)
+            {
+                Content = UnitTestSystem.CreateTestPage();
+                IMobileTestPage imtp = Content as IMobileTestPage;
+                if (imtp != null)
+                {
+                    BackKeyPress += (x, xe) => xe.Cancel = imtp.NavigateBack();
+                }
+            }
+        }
+    }
+}
