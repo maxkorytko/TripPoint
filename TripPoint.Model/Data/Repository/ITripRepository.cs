68a23cb0eb8e86916f05ece6b3cd23f6e62a9ce1
MaxK <maxk2005@gmail.com>
2012-06-09 15:25:54 -0700

    Add more funtionality

--- TripPoint.Model/Data/Repository/ITripRepository.cs
+++ TripPoint.Model/Data/Repository/ITripRepository.cs
@@ -5,9 +5,18 @@ namespace TripPoint.Model.Data.Repository
     public interface ITripRepository
     {
         /// <summary>
-        /// Persists trip to the data store
+        /// Creates a trip in the data store
+        /// Updates the trip if it already exists
         /// </summary>
         /// <param name="trip">Trip object to persist</param>
         void SaveTrip(Trip trip);
+
+        Trip FindTrip(string tripID);
+
+        void DeleteTrip(Trip trip);
+
+        void DeleteAll();
+
+        int Count { get; }
     }
 }
--- TripPoint.Model/Data/Repository/Memory/MemoryTripRepository.cs
+++ TripPoint.Model/Data/Repository/Memory/MemoryTripRepository.cs
@@ -1,6 +1,7 @@
 ﻿#region SDK Usings
 
 using System;
+using System.Collections.Generic;
 
 #endregion
 
@@ -10,10 +11,65 @@ namespace TripPoint.Model.Data.Repository.Memory
 {
     public class MemoryTripRepository : ITripRepository
     {
+        private static IDictionary<string, Trip> _dataStore = new Dictionary<string, Trip>();
+
         public void SaveTrip(Trip trip)
         {
             if (trip == null)
                 throw new ArgumentNullException("trip");
+
+            _dataStore[trip.ID] = trip;
+
+            //if (TripExists(trip))
+            //{
+            //    UpdateTrip(trip);
+            //    return;
+            //}
+
+            //CreateTrip(trip);
+        }
+
+        //private static bool TripExists(Trip trip)
+        //{
+        //    return _dataStore.ContainsKey(trip.ID);
+        //}
+
+        //private static void CreateTrip(Trip trip)
+        //{
+        //    _dataStore.Add(trip.ID, trip);
+        //}
+
+        //private static void UpdateTrip(Trip trip)
+        //{
+        //    _dataStore[trip.ID] = trip;
+        //}
+
+        public Trip FindTrip(string tripID)
+        {
+            Trip trip = null;
+
+            if (_dataStore.ContainsKey(tripID))
+                trip = _dataStore[tripID];
+
+            return trip;
+        }
+
+        public void DeleteTrip(Trip trip)
+        {
+            _dataStore.Remove(trip.ID);
+        }
+
+        public void DeleteAll()
+        {
+            _dataStore.Clear();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _dataStore.Count;
+            }
         }
     }
 }
