19da476807252f8a70d2e34419716df9e1dfd453
MaxK <maxk2005@gmail.com>
2012-06-14 21:52:59 -0700

    Add dummy data to memory trip repository

--- TripPoint.Model/Data/Repository/Memory/MemoryTripRepository.cs
+++ TripPoint.Model/Data/Repository/Memory/MemoryTripRepository.cs
@@ -11,7 +11,54 @@ namespace TripPoint.Model.Data.Repository.Memory
 {
     public class MemoryTripRepository : ITripRepository
     {
-        private static IDictionary<string, Trip> _dataStore = new Dictionary<string, Trip>();
+        private static IDictionary<string, Trip> _dataStore = CreateDataStore();
+
+        #region Dummy Data
+        private static IDictionary<string, Trip> CreateDataStore()
+        {
+            var dataStore = new Dictionary<string, Trip>();
+
+            var trip = new Trip
+            {
+                Name = "Cabbot Trail",
+                Notes = "Driving along Cabbot Trail in Nova Scotia",
+                StartDate = DateTime.Now.AddDays(-4)
+            };
+            trip.Checkpoints.Add(new Checkpoint
+            {
+                Title = "Pearson in Toronto",
+                Notes = "Minutes before departure"
+            });
+            trip.Checkpoints.Add(new Checkpoint
+            {
+                Title = "Going to Cabbot Trail"
+            });
+            trip.Checkpoints.Add(new Checkpoint
+            {
+                Title = "enjoying the scenery",
+                Notes = "50 km along the trail"
+            });
+            dataStore[trip.ID] = trip;
+
+            trip = new Trip
+            {
+                Name = "Hiking in Milton area",
+                StartDate = DateTime.Now.AddDays(-25),
+                EndDate = DateTime.Now.AddDays(-23)
+            };
+            trip.Checkpoints.Add(new Checkpoint
+            {
+                Title = "at the beginning of the trail"
+            });
+            trip.Checkpoints.Add(new Checkpoint
+            {
+                Title = "resting"
+            });
+            dataStore[trip.ID] = trip;
+
+            return dataStore;
+        }
+        #endregion
 
         public void SaveTrip(Trip trip)
         {
