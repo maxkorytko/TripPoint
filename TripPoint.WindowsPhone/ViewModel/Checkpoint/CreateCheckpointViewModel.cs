02bc59920759703b40f4b979783026b7cff759bd
MaxK <maxk2005@gmail.com>
2012-06-23 12:24:13 -0400

    Hook up action bar buttons with the view model on the create checkpoint view

--- TripPoint.WindowsPhone/ViewModel/Checkpoint/CreateCheckpointViewModel.cs
+++ TripPoint.WindowsPhone/ViewModel/Checkpoint/CreateCheckpointViewModel.cs
@@ -1,8 +1,39 @@
-﻿
+﻿#region SDK Usings
+
+using System.Windows.Input;
+
+#endregion
+
+using GalaSoft.MvvmLight.Command;
+using TripPoint.Model.Utils;
+
 namespace TripPoint.WindowsPhone.ViewModel.Checkpoint
 {
     public class CreateCheckpointViewModel : TripPointViewModelBase
     {
+        public CreateCheckpointViewModel()
+        {
+            InitializeCommands();
+        }
 
+        private void InitializeCommands()
+        {
+            CreateCheckpointCommand = new RelayCommand(CreateCheckpointAction);
+            CancelCreateCheckpointCommand = new RelayCommand(CancelCreateCheckpointAction);
+        }
+
+        public ICommand CreateCheckpointCommand { get; private set; }
+
+        public ICommand CancelCreateCheckpointCommand { get; private set; }
+
+        private void CreateCheckpointAction()
+        {
+            Logger.Log(this, "CreateCheckpoint");
+        }
+
+        private void CancelCreateCheckpointAction()
+        {
+            Logger.Log(this, "CancelCreateCheckpoint");
+        }
     }
 }
