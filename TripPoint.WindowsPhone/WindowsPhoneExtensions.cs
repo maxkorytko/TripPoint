a61ff07cf314f6ca00249ac8504bb7674069f8e8
MaxK <mkorytko@panvistamedia.com>
2012-06-28 08:28:51 -0400

    Add extensions class for TripPoint.WindowsPhone project

---
+++ TripPoint.WindowsPhone/WindowsPhoneExtensions.cs
@@ -0,0 +1,29 @@
+﻿#region SDK Usings
+using System;
+using System.Windows;
+using Microsoft.Phone.Controls;
+#endregion
+
+using TripPoint.Model.Utils;
+
+namespace TripPoint.WindowsPhone
+{
+    public static class WindowsPhoneExtensions
+    {
+        public static string TryGetQueryStringParameter(this PhoneApplicationPage page, string parameterName)
+        {
+            try
+            {
+                var value = page.NavigationContext.QueryString[parameterName];
+                return value;
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(string.Format("TryGetQueryStringParameter for parameter '{0}' has failed. {1}",
+                    parameterName, ex.Message));
+
+                return string.Empty;
+            }
+        }
+    }
+}
