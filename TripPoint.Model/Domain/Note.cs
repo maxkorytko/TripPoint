014ef13377c37c64aa3f73051365c272c7fb35bc
MaxK <maxk2005@gmail.com>
2012-06-30 10:52:26 -0400

    Create the Note class

---
+++ TripPoint.Model/Domain/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TripPoint.Model.Domain
+{
+    /// <summary>
+    /// A note with the ability to track the time and date it was taken on
+    /// </summary>
+    public class Note
+    {
+        public string Text { get; set; }
+
+        /// <summary>
+        /// The time and date the note was creted on
+        /// </summary>
+        public DateTime TimeStamp { get; set; }
+    }
+}
