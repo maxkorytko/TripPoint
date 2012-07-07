9d797c45db1033bfbb4ed4a6e345b30e343229d9
MaxK <maxk2005@gmail.com>
2012-07-07 18:02:24 -0400

    Add LINQ data mappings to the Checkpoint class

--- TripPoint.Model/Domain/Checkpoint.cs
+++ TripPoint.Model/Domain/Checkpoint.cs
@@ -1,41 +1,188 @@
 ﻿using System;
-using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
 
 namespace TripPoint.Model.Domain
 {
     /// <summary>
     /// Represents a checkpoint along the trip route
     /// </summary>
-    public class Checkpoint
+    [Table]
+    public class Checkpoint : INotifyPropertyChanging, INotifyPropertyChanged
     {
+        private int _checkpointID;
+        private DateTime _timestamp;
+        private String _title;
+        
+        private EntitySet<Note> _notes;
+        private EntityRef<Trip> _trip;
+
         public Checkpoint()
         {
             Timestamp = DateTime.Now;
             Location = new GeoLocation();
-            Notes = new ObservableCollection<Note>();
+            Notes = new EntitySet<Note>(
+                AddNoteAction, RemoveNoteAction
+                );
         }
+
+        #region ID
+        [Column(
+            IsPrimaryKey = true,
+            IsDbGenerated = true,
+            DbType = "INT NOT NULL Identity",
+            CanBeNull = false,
+            AutoSync = AutoSync.OnInsert
+        )]
+        public int ID
+        {
+            get
+            {
+                return _checkpointID;
+            }
+            set
+            {
+                if (_tripID != value)
+                {
+                    NotifyPropertyChanging("ID");
+                    _tripID = value;
+                    NotifyPropertyChanged("ID");
+                }
+            }
+        }
+        #endregion
 
         /// <summary>
         /// Geographical location of the checkpoint
         /// </summary>
         public GeoLocation Location { get; set; }
 
+        #region Timestamp
         /// <summary>
         /// Checkpoint creation time and date
         /// </summary>
-        public DateTime Timestamp { get; set; }
+        [Column]
+        public DateTime Timestamp 
+        {
+            get { return _timestamp; }
+            set
+            {
+                if (_timestamp == value) return;
 
-        public string Title { get; set; }
+                NotifyPropertyChanging("Timestamp");
+                _timestamp = value;
+                NotifyPropertyChanged("Timestamp");
+            }
+        }
+        #endregion
 
-        /// <summary>
-        /// A list of notes
-        /// </summary>
-        public ObservableCollection<Note> Notes { get; set; }
+        #region Title
+        [Column]
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (_title == value) return;
+
+                NotifyPropertyChanging("Title");
+                _title = value;
+                NotifyPropertyChanged("Title");
+            }
+        }
+        #endregion
+
+        #region Notes
+        [Association(Storage = "_notes", OtherKey = "_checkpointID", ThisKey = "ID")]
+        public EntitySet<Note> Notes
+        {
+            get { return _notes; }
+            set
+            {
+                _notes.Assign(value);
+            }
+        }
+
+        // Called during an add operation
+        private void AddNoteAction(Note note)
+        {
+            NotifyPropertyChanging("Note");
+            note.Checkpoint = this;
+        }
+
+        // Called during a remove operation
+        private void RemoveNoteAction(Note note)
+        {
+            NotifyPropertyChanging("Note");
+            note.Checkpoint = null;
+        }
+        #endregion
+
+        #region Trip
+        [Column]
+        internal int _tripID;
+
+        [Association(
+            Storage = "_trip",
+            ThisKey = "_tripID",
+            OtherKey = "ID",
+            IsForeignKey = true
+        )]
+        public Trip Trip
+        {
+            get { return _trip.Entity; }
+            set
+            {
+                NotifyPropertyChanging("Trip");
+                _trip.Entity = value;
+
+                if (value != null)
+                    _tripID = value.ID;
+
+                NotifyPropertyChanged("Trip");
+            }
+        }
+        #endregion
+
+        #region Version
+        [Column(IsVersion = true)]
+        private Binary _version;
+        #endregion
 
         public override string ToString()
         {
             return string.Format("{0} at {1}. Location: {2}",
                 Title, Timestamp.TimeOfDay, Location);
         }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanging Members
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        private void NotifyPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
     }
 }
