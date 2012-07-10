c4bf54f1e3500c702adc50b0fc29c1baf9ba1205
MaxK <maxk2005@gmail.com>
2012-07-09 21:17:05 -0400

    Create TripPointDataContext class

---
+++ TripPoint.Model/Data/TripPointDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Linq;
+
+using TripPoint.Model.Domain;
+
+namespace TripPoint.Model.Data
+{
+    public class TripPointDataContext : DataContext
+    {
+        public TripPointDataContext(string connectionString)
+            : base(connectionString)
+        { }
+
+        public Table<Trip> Trips;
+
+        public Table<Checkpoint> Checkpoints;
+    }
+}
