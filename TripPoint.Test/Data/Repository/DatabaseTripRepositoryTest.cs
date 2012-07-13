86279d565bf4ba2f4363d4feb81e7f74fc72cacb
MaxK <maxk2005@gmail.com>
2012-07-12 21:04:39 -0400

    Unit test DatabaseTripRepository class

---
+++ TripPoint.Test/Data/Repository/DatabaseTripRepositoryTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Linq;
+
+using TripPoint.Model.Domain;
+using TripPoint.Model.Data.Repository;
+
+namespace TripPoint.Test.Data.Repository
+{
+    [TestClass]
+    public class DatabaseTripRepositoryTest
+    {
+        [TestMethod]
+        public void TestSaveTrip()
+        {
+            
+        }
+    }
+}
