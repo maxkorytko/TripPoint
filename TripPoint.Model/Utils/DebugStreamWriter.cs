3d17945cc8cb061bd4cfa6bab2cbe8560628a3b6
MaxK <maxk2005@gmail.com>
2012-07-22 19:55:46 -0400

    Create DebugStreamWriter class

---
+++ TripPoint.Model/Utils/DebugStreamWriter.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace TripPoint.Model.Utils
+{
+    /// <summary>
+    /// Writer for the System.Diagnostics.Debug stream
+    /// </summary>
+    public class DebugStreamWriter : TextWriter
+    {
+        private const int DefaultBufferSize = 256;
+        private StringBuilder _buffer;
+
+        public DebugStreamWriter()
+        {
+            BufferSize = 8;//256;
+            _buffer = new StringBuilder(BufferSize);
+        }
+
+        public int BufferSize
+        {
+            get;
+            private set;
+        }
+
+        public override System.Text.Encoding Encoding
+        {
+            get { return Encoding.UTF8; }
+        }
+
+        #region StreamWriter Overrides
+        public override void Write(char value)
+        {
+            _buffer.Append(value);
+            if (_buffer.Length >= BufferSize)
+                Flush();
+        }
+
+        public override void WriteLine(string value)
+        {
+            Flush();
+
+            using(var reader = new StringReader(value))
+            {
+                string line; 
+                while( null != (line = reader.ReadLine()))
+                    Debug.WriteLine(line);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                Flush();
+        }
+
+        public override void Flush()
+        {
+            if (_buffer.Length > 0)
+            {
+                Debug.WriteLine(_buffer);
+                _buffer.Clear();
+            }
+        }
+        #endregion
+    }
+}
