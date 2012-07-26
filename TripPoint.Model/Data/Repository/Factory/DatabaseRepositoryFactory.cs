99dcee5f0ac86d12bd10ffe71cd9c50ab5b638cb
MaxK <mkorytko@panvistamedia.com>
2012-07-26 08:40:20 -0400

    Create DatabaseRepositoryFactory class

---
+++ TripPoint.Model/Data/Repository/Factory/DatabaseRepositoryFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace TripPoint.Model.Data.Repository.Factory
+{
+    public class DatabaseRepositoryFactory : IRepositoryFactory
+    {
+        private TripPointDataContext _dataContext;
+
+        private TripPointDataContext DataContext
+        {
+            get
+            {
+                if (_dataContext != null)
+                    _dataContext.Dispose();
+
+                _dataContext = new TripPointDataContext(TripPointDataContext.ConnectionString);
+
+                return _dataContext;
+            }
+        }
+
+        public ITripRepository TripRepository
+        {
+            get
+            {
+                return new DatabaseTripRepository(DataContext);
+            }
+        }
+    }
+}
