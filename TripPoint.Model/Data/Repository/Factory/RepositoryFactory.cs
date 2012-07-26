28dfe8153205645a331bfd50f1d8a407c3292af5
MaxK <mkorytko@panvistamedia.com>
2012-07-26 08:41:00 -0400

    Create RepositoryFactory class which serves as a static gateway

---
+++ TripPoint.Model/Data/Repository/Factory/RepositoryFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TripPoint.Model.Data.Repository.Factory
+{
+    /// <summary>
+    /// Gateway for obtaining an instance of IRepositoryFactory
+    /// </summary>
+    public class RepositoryFactory
+    {
+        private static IRepositoryFactory _repositoryFactory;
+
+        /// <summary>
+        /// Wires up an implementation of IRepositoryFactory
+        /// </summary>
+        /// <param name="repositoryFactory"></param>
+        public static void Initialize(IRepositoryFactory repositoryFactory)
+        {
+            if (repositoryFactory == null)
+                throw new ArgumentNullException("repositoryFactory");
+
+            _repositoryFactory = repositoryFactory;
+        }
+
+        public static IRepositoryFactory Create()
+        {
+            return _repositoryFactory;
+        }
+    }
+}
