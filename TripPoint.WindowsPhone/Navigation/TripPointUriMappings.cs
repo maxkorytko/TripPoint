cef757308edecca02ca1f759d28ba2015c04c78b
MaxK <maxk2005@gmail.com>
2012-07-26 21:33:58 -0400

    Create TripPointUriMappings class

---
+++ TripPoint.WindowsPhone/Navigation/TripPointUriMappings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Navigation;
+
+namespace TripPoint.WindowsPhone.Navigation
+{
+    /// <summary>
+    /// Application URI mappings
+    /// </summary>
+    public class TripPointUriMappings
+    {
+        private static UriMapping[] _uriMappings = new UriMapping[]
+        {
+            new UriMapping
+            {
+                Uri = new Uri("/Trips", UriKind.Relative),
+                MappedUri = new Uri("/View/Trip/TripListView.xaml", UriKind.Relative)
+            },
+            new UriMapping
+            {
+                Uri = new Uri("/Trip/Create", UriKind.Relative),
+                MappedUri = new Uri("/View/Trip/CreateTripView.xaml", UriKind.Relative)
+            },
+            new UriMapping
+            {
+                Uri = new Uri("/Trip/Current", UriKind.Relative),
+                MappedUri = new Uri("/View/Trip/CurrentTripView.xaml", UriKind.Relative)
+            },
+            new UriMapping
+            {
+                Uri = new Uri("/Trip/{tripID}/Details", UriKind.Relative),
+                MappedUri = new Uri("/View/Trip/TripDetailsView.xaml?tripID={tripID}", UriKind.Relative)
+            },
+            new UriMapping
+            {
+                Uri = new Uri("/Trip/{tripID}/Checkpoints/Create", UriKind.Relative),
+                MappedUri = new Uri("/View/Checkpoint/CreateCheckpointView.xaml?tripID={tripID}", UriKind.Relative)
+            },
+            new UriMapping
+            {
+                Uri = new Uri("/Trip/{tripID}/Checkpoints/{checkpointIndex}/Add/Notes", UriKind.Relative),
+                MappedUri = new Uri("/View/Checkpoint/CheckpointAddNotesView.xaml?tripID={tripID}&checkpointIndex={checkpointIndex}",
+                    UriKind.Relative)
+            }
+        };
+
+        public static UriMapping[] UriMappings
+        {
+            get { return _uriMappings; }
+        }
+    }
+}
