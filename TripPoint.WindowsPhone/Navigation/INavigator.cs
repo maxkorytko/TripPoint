1e2f676ac38fd41072589e0e90edbd99603eae7a
MaxK <maxk2005@gmail.com>
2012-07-26 21:51:44 -0400

    Create and INavigator interface
    Create TripPointNavigator class

---
+++ TripPoint.WindowsPhone/Navigation/INavigator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TripPoint.WindowsPhone.Navigation
+{
+    /// <summary>
+    /// Navigation facade for the application
+    /// </summary>
+    public interface INavigator
+    {
+        /// <summary>
+        /// Navigates to a given URI
+        /// </summary>
+        /// <param name="uri">URI to navigate to</param>
+        /// <returns>true if navigation is successful</returns>
+        bool Navigate(string uri);
+
+        /// <summary>
+        /// Navigates to a previous page if possible
+        /// </summary>
+        void GoBack();
+    }
+}
---
+++ TripPoint.WindowsPhone/Navigation/TripPointNavigator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Phone.Controls;
+
+namespace TripPoint.WindowsPhone.Navigation
+{
+    public class TripPointNavigator : INavigator
+    {
+        PhoneApplicationFrame _rootFrame;
+
+        public TripPointNavigator()
+        {
+            _rootFrame = (Application.Current as App).RootFrame;
+
+            if (_rootFrame == null)
+                throw new InvalidOperationException("Application RootFrame is null");
+        }
+
+        public bool Navigate(string uri)
+        {
+            return _rootFrame.Navigate(new Uri(uri, UriKind.Relative));
+        }
+
+        public void GoBack()
+        {
+            if (_rootFrame.CanGoBack)
+                _rootFrame.GoBack();
+        }
+    }
+}
