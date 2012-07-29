b0c62a199869b3836e5c8f1fb103b54841af8e58
MaxK <maxk2005@gmail.com>
2012-07-29 18:17:45 -0400

    Add CheckpointDetailsViewModel class

---
+++ TripPoint.WindowsPhone/ViewModel/CheckpointDetailsViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+using TripPoint.Model.Domain;
+using TripPoint.Model.Data.Repository.Factory;
+using TripPoint.Model.Utils;
+using TripPoint.WindowsPhone.ViewModel;
+
+namespace TripPoint.WindowsPhone.ViewModel
+{
+    public class CheckpointDetailsViewModel : TripPointViewModelBase
+    {
+        public CheckpointDetailsViewModel(IRepositoryFactory repositoryFactory)
+            : base(repositoryFactory)
+        {
+            Checkpoint = new Checkpoint();
+        }
+
+        public Checkpoint Checkpoint { get; private set; }
+
+        public override void OnNavigatedTo(Navigation.TripPointNavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            Logger.Log(this, "checkpoint ID: {0}", e.View.TryGetQueryStringParameter("checkpointID"));
+        }
+    }
+}
