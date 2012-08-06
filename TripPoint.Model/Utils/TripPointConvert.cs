ecb47527109f1a650d8a4cbc56d874a179e6225c
MaxK <maxk2005@gmail.com>
2012-08-06 19:47:16 -0400

    Create TripPointConvert class

---
+++ TripPoint.Model/Utils/TripPointConvert.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TripPoint.Model.Utils
+{
+    public class TripPointConvert
+    {
+        public static string ToString(int value)
+        {
+            string convertedValue;
+
+            try
+            {
+                convertedValue = Convert.ToString(value);
+            }
+            catch (Exception)
+            {
+                convertedValue = string.Empty;
+            }
+
+            return convertedValue;
+        }
+
+        public static int ToInt32(string value)
+        {
+            int convertedValue;
+
+            try
+            {
+                convertedValue = Convert.ToInt32(value);
+            }
+            catch (Exception)
+            {
+                convertedValue = -1;
+            }
+
+            return convertedValue;
+        }
+    }
+}
