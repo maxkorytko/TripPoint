e181d2f86d108345f2e2978866ea0c24cb4d6f44
MaxK <maxk2005@gmail.com>
2012-09-09 13:16:08 -0400

    Create CountdownTimer class

---
+++ TripPoint.Test/Utils/CountdownTimerTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TripPoint.WindowsPhone.Utils;
+
+namespace TripPoint.Test.Utils
+{
+    [TestClass]
+    public class CountdownTimerTest
+    {
+        private CountdownTimer _timer;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            _timer = new CountdownTimer();
+        }
+
+        [TestCleanup]
+        public void CleanupTest()
+        {
+            _timer.Stop();
+            _timer = null;
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void TestCountdown()
+        {
+            int counter = 0;
+
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.OnExplode = () =>
+            {
+                lock (_timer)
+                {
+                    counter++;
+                    Monitor.Pulse(_timer);
+                }
+            };
+
+            _timer.Start();
+
+            // _timer.OnExposed never has a chance to run before Wait times out
+            // TODO: figure out how to run the timer on a separate thread
+            lock (_timer)
+            {
+                Monitor.Wait(_timer, 3000);
+            }
+
+            Assert.AreEqual<int>(1, counter, "The timer must explode exactly once");
+        }
+    }
+}
---
+++ TripPoint.WindowsPhone/Utils/CountdownTimer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace TripPoint.WindowsPhone.Utils
+{
+    public class CountdownTimer
+    {
+        private DispatcherTimer _timer;
+        private EventHandler _timerTickEventHandler;
+
+        public delegate void Explode();
+
+        public CountdownTimer()
+        {
+            _timer = new DispatcherTimer();
+
+            _timerTickEventHandler = (sender, args) =>
+            {
+                Stop();
+
+                if (OnExplode != null)
+                    OnExplode();
+            };
+        }
+
+        public TimeSpan Interval
+        {
+            get { return _timer.Interval; }
+            set { _timer.Interval = value; }
+        }
+
+        public Explode OnExplode { get; set; }
+
+        public void Start()
+        {
+            _timer.Tick += _timerTickEventHandler;
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+            _timer.Tick -= _timerTickEventHandler;
+        }
+    }
+}
