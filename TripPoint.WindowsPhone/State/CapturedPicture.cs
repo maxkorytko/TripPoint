88a15701f9e285d410e78feee24e59980ca6a3d7
MaxK <maxk2005@gmail.com>
2012-10-28 17:17:58 -0400

    Implement the CapturedPicture class

--- TripPoint.WindowsPhone/State/CapturedPicture.cs
+++ TripPoint.WindowsPhone/State/CapturedPicture.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using System.Runtime.Serialization;
+
+using TripPoint.Model.Utils;
 
 namespace TripPoint.WindowsPhone.State
 {
+    [DataContract]
     public class CapturedPicture
     {
+        private byte[] _picture;
+
         public CapturedPicture(Stream picture)
         {
-
+            Picture = TripPointConvert.ToBytes(picture);
         }
+
+        [DataMember]
+        public byte[] Picture { get; set; }
     }
 }
