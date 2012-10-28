5769ac3df925cb39048fe362c775663058a02606
MaxK <maxk2005@gmail.com>
2012-10-28 17:16:59 -0400

    Add ToBytes method to the TripPointConvert class
    Add comments

--- TripPoint.Model/Utils/TripPointConvert.cs
+++ TripPoint.Model/Utils/TripPointConvert.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.IO;
 
 namespace TripPoint.Model.Utils
 {
+    /// <summary>
+    /// Utility class for converting between various types
+    /// </summary>
     public class TripPointConvert
     {
+        /// <summary>
+        /// Converts an integer to a string
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Empty string if conversion is not possible</returns>
         public static string ToString(int value)
         {
             string convertedValue;
@@ -20,6 +29,11 @@ namespace TripPoint.Model.Utils
             return convertedValue;
         }
 
+        /// <summary>
+        /// Converts a string to an integer
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>-1 if conversion is not possible</returns>
         public static int ToInt32(string value)
         {
             int convertedValue;
@@ -34,6 +48,21 @@ namespace TripPoint.Model.Utils
             }
 
             return convertedValue;
+        }
+
+        /// <summary>
+        /// Creates a byte array and populates it with bytes from the given stream
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static byte[] ToBytes(Stream stream)
+        {
+            if (stream == null || !stream.CanRead)
+                return new byte[0];
+
+            byte[] bytes = new byte[stream.Length];
+            stream.Read(bytes, 0, bytes.Length);
+            return bytes;
         }
     }
 }
