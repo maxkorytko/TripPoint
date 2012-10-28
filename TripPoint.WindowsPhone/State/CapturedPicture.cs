c05530a888a95cc3428f0395863d49486af86ec0
MaxK <maxk2005@gmail.com>
2012-10-28 14:39:23 -0400

    Create CapturedPicture class

---
+++ TripPoint.WindowsPhone/State/CapturedPicture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace TripPoint.WindowsPhone.State
+{
+    public class CapturedPicture
+    {
+        public CapturedPicture(Stream picture)
+        {
+
+        }
+    }
+}
