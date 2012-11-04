b4a140201e8a645901f570224e0224cece5f7601
MaxK <maxk2005@gmail.com>
2012-11-04 14:00:38 -0500

    Add and implement ImageUtils class
    Add dependencies

---
+++ TripPoint.WindowsPhone/Utils/ImageUtils.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+using TripPoint.WindowsPhone.Utils.ExifLib;
+
+namespace TripPoint.WindowsPhone.Utils
+{
+    /// <summary>
+    /// Represents a collection of utility methods for working with images
+    /// </summary>
+    public class ImageUtils
+    {
+        /// <summary>
+        /// Rotates the photo stream to portrait orientation
+        /// Use this method when you have a photo, which is not in portrait orientation,
+        /// but you want to display it in portrait orientation
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static Stream RotateImageToPortrait(Stream source)
+        {
+            var result = new MemoryStream();
+
+            try
+            {
+                var initialSourceStreamPosition = source.Position;
+
+                source.Position = 0;
+                
+                ExifOrientation orientation = ExifReader.ReadJpeg(source).Orientation;
+                
+                var degreesToPortrait = GetDegreesToPortrait(orientation);
+                var bitmap = CreateWriteableBitmapFromStream(source);
+                var rotatedBitmap = bitmap.Rotate(degreesToPortrait);
+
+                source.Position = initialSourceStreamPosition;
+
+                return rotatedBitmap.SaveJpeg();
+            }
+            catch (Exception)
+            {
+                return new MemoryStream();
+            }
+        }
+
+        /// <summary>
+        /// Determines the number of degrees between current and portrait orientations
+        /// </summary>
+        /// <param name="orientation"></param>
+        /// <returns></returns>
+        private static int GetDegreesToPortrait(ExifOrientation orientation)
+        {
+            switch (orientation)
+            {
+                case ExifOrientation.TopRight:
+                    return 90;
+                case ExifOrientation.BottomRight:
+                    return 180;
+                case ExifOrientation.BottomLeft:
+                    return 270;
+                case ExifOrientation.TopLeft:
+                case ExifOrientation.Undefined:
+                default:
+                    return 0;
+            }
+        }
+
+        /// <summary>
+        /// Shorthand method for creating a WriteableBitmap object from a photo stream
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static WriteableBitmap CreateWriteableBitmapFromStream(Stream source)
+        {
+            var bitmap = new BitmapImage();
+            bitmap.SetSource(source);
+
+            return new WriteableBitmap(bitmap);
+        }
+    }
+}
--- TripPoint.WindowsPhone/WindowsPhoneExtensions.cs
+++ TripPoint.WindowsPhone/WindowsPhoneExtensions.cs
@@ -1,6 +1,7 @@
 ﻿#region SDK Usings
 using System;
-using System.Windows;
+using System.IO;
+using System.Windows.Media.Imaging;
 using Microsoft.Phone.Controls;
 #endregion
 
@@ -24,6 +25,26 @@ namespace TripPoint.WindowsPhone
 
                 return string.Empty;
             }
+        }
+
+        public static Stream SaveJpeg(this WriteableBitmap bitmap)
+        {
+            MemoryStream stream = new MemoryStream();
+
+            try
+            {
+                bitmap.SaveJpeg(stream, bitmap.PixelWidth, bitmap.PixelHeight, 0, 100);
+            }
+            catch
+            {
+                // nothing
+            }
+            finally
+            {
+                stream.Position = 0;
+            }
+
+            return stream;
         }
     }
 }
